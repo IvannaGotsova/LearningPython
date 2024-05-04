d5b3369d068be448d68455312b3e2ff42676ce1a
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-04 18:44:06 +0300

    Operators

---
+++ Operators.py
@@ -0,0 +1,28 @@
+x = 3
+y = 5
+
+x %= 3
+print(x)
+
+x //= 3
+print(x)
+
+x **= 3
+print(x)
+
+x &= 3
+print(x)
+
+x |= 3
+print(x)
+
+x ^= 3
+print(x)
+
+x >>= 3
+print(x)
+
+x <<= 3
+print(x)
+
+print(x := 3)
