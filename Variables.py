fe422ccab4f70353e12e33fdf8b2094d027c9091
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-04 18:17:29 +0300

    Variables

--- Variables.py
+++ Variables.py
@@ -5,12 +5,17 @@ y = 5.5
 z = "string"
 
 x1, x2, x3 = "One", "Two", "Three"
+x4 = x5 = x6 = "Number"
+
 print(x)
 print(y)
 print(z)
 print(x1)
 print(x2)
 print(x3)
+print(x4)
+print(x5)
+print(x6)
 
 print(type(x))
 print(type(y))
