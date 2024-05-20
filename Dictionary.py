1ce93e9f28ff7281a56ad0c1dd0db58492b0477e
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-20 17:52:58 +0300

    Dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -7,3 +7,12 @@ dictionaryExample = {
 print(dictionaryExample)
 print(dictionaryExample["firstName"])
 
+print(len(dictionaryExample))
+print(type(dictionaryExample))
+
+print(dictionaryExample.keys())
+print(dictionaryExample.values())
+
+dictionaryExample['city'] = "Sofia"
+print(dictionaryExample)
+
