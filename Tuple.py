a4317318084c9c068fc280fd7d7e8aec1c279f35
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-20 17:06:27 +0300

    Tuple

--- Tuple.py
+++ Tuple.py
@@ -2,3 +2,9 @@ tupleExample = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 1
 
 print(tupleExample[4])
 print(len(tupleExample))
+
+tupleToAdd = (21, 22, 23, 24, 25)
+tupleExample += tupleToAdd
+
+print(tupleExample)
+
