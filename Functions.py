115aa564a9120b6a9906e78576426088082ee5e5
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-20 19:21:04 +0300

    Functions

--- Functions.py
+++ Functions.py
@@ -1,6 +1,14 @@
+def helloFunction():
+  print("Hello")
+
 x = "Ivan"
 
 def exampleFunction():
   print("My name is " + x)
 
 exampleFunction()
+
+def exampleFunctionParam(x):
+  print("My name is " + x)
+
+exampleFunctionParam(x)
