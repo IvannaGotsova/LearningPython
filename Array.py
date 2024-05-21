3fde92970311b1e470e7cb4258efdb4453e0ef35
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-21 14:55:03 +0300

    Array

--- Array.py
+++ Array.py
@@ -4,3 +4,10 @@ arrayExample = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 print(len(arrayExample))
 print(type(arrayExample))
+
+print(arrayExample[3])
+
+arrayExampleToAdd = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+arrayExampleAdded = np.append(arrayExample, arrayExampleToAdd)
+print(arrayExampleAdded)
