3b045d35802d979f898c69281a9ded73f28d82e4
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-21 17:58:58 +0300

    Input

---
+++ iNPUT.py
@@ -0,0 +1,14 @@
+firstName = input("What is your first name ")
+print("Your first name is " + firstName)
+
+lastName = input("What is your last name ")
+print("Your last name is: " + lastName)
+
+age = input("What is your age ")
+print("Your age is: " + age)
+
+city = input("What is your city ")
+print("Your city is: " + city)
+
+salary = input("What is your salary " )
+print("Your salary is: " + salary)
