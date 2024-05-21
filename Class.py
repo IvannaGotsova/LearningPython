f5066257357f1ee77379ca6cf354ccc29774e2fd
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-21 15:35:00 +0300

    Class

--- Class.py
+++ Class.py
@@ -8,3 +8,20 @@ person = Person()
 print(person.firstname)
 print(person.lastname)
 print(person.age)
+
+class PersonOne:
+    def __init__(personOne, firstname, lastname, age):
+        personOne.firstname = firstname
+        personOne.lastname = lastname
+        personOne.age = age
+
+    def __str__(personOne):
+        return f"{personOne.firstname} {personOne.lastname},  {personOne.age} years old."
+
+personOne = PersonOne("Petar", "Petrov", 44)
+
+print(personOne.firstname)
+print(personOne.lastname)
+print(personOne.age)
+print(personOne)
+
