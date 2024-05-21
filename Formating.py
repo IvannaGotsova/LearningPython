87f8af0b757e2b6790c68af6f181056aef1530d3
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-21 18:27:53 +0300

    Format

--- Formating.py
+++ Formating.py
@@ -5,5 +5,7 @@ city = "Sofia"
 salary = 1000
 
 textToFormat = f"{firstname} {lastName}, {age} years old, from {city} - {salary:.2f}"
-
 print(textToFormat)
+
+textToFormat = "{0} {1}, {2} years old, from {3} - {4:.2f}"
+print(textToFormat.format(firstname, lastName, age, city, salary))
