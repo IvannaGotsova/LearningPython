857bee10ef3b2cf87cf5880e710526306f2f25e6
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-21 18:09:57 +0300

    Format

---
+++ Formating.py
@@ -0,0 +1,9 @@
+firstname = "Ivan"
+lastName = "Ivanov"
+age = 44
+city = "Sofia"
+salary = 1000
+
+textToFormat = f"{firstname} {lastName}, {age} years old, from {city} - {salary:.2f}"
+
+print(textToFormat)
