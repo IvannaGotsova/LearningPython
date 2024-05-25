dbd9c1da7f16dd1d801b8a894f7141eb524aff7a
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-25 16:02:58 +0300

    Type casting

---
+++ Type casting.py
@@ -0,0 +1,14 @@
+intVariable = 4
+floatVariable = 4.4
+stringVariable = '4'
+
+print("Everything is int: ")
+print(intVariable + int(floatVariable) + int(stringVariable))
+print()
+
+print("Everything is float: ")
+print(float(intVariable) + floatVariable + float(stringVariable))
+print()
+
+print("Everything is string: ")
+print(str(intVariable) + str(floatVariable) + stringVariable)
