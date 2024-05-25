7b7a4dd95b9a1e0d673df00b418db2f139358464
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-25 16:02:40 +0300

    Multiple assignment

---
+++ Multiple assignment.py
@@ -0,0 +1,10 @@
+first_name, second_name, third_name, fourth_name, fifth_name, sixth_name = "Ivan","Peter","Dimitar", "Stoqn", "Philip", "Stephan"
+
+first_age = second_age = third_age = fourth_age = fifth_age = sixth_age = 40
+
+print(first_name, first_age)
+print(second_name, second_age)
+print(third_name, third_age)
+print(fourth_name, fourth_age)
+print(fifth_name, fifth_age)
+print(sixth_name, sixth_age)
