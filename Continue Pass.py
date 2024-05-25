2ebe968e0234302bbc86446b424da2c487787812
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-25 18:06:15 +0300

    Continue Pass

---
+++ Continue Pass.py
@@ -0,0 +1,12 @@
+numbersExample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for x in numbersExample:
+    if x == 2:
+        continue
+    print(x)
+
+
+for x in numbersExample:
+    if x == 2:
+        pass
+    print(x)
