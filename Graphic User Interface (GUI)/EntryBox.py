c6515aaf8dc34005e91b882f973ead43ed16d4a7
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-28 15:17:35 +0300

    Entry Box Modified

--- Graphic User Interface (GUI)/EntryBox.py
+++ Graphic User Interface (GUI)/EntryBox.py
@@ -3,5 +3,7 @@ from tkinter import *
 
 windowExample = Tk()
 
+entryBoxExample = Entry(font=("Ariel", 22, "italic"), fg="blue", bg="black", show="!")
+entryBoxExample.pack()
 
 windowExample.mainloop()
