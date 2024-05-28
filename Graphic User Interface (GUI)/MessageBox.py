304cdbdcc9e14f58adbd9578722d01d35c905bed
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-28 18:27:18 +0300

    Message Box Modified

--- Graphic User Interface (GUI)/MessageBox.py
+++ Graphic User Interface (GUI)/MessageBox.py
@@ -2,13 +2,49 @@ from tkinter import *
 from tkinter import messagebox
 
 
-def click():
-    messagebox.showinfo(title="Title", message="This is message box!")
+def info():
+    messagebox.showinfo(title="Info", message="This is info message box!")
+
+
+def warning():
+    messagebox.showwarning(title="Warning", message="This is warning message box!")
+
+
+def error():
+    messagebox.showerror(title="Error", message="This is error message box!")
+
+
+def yesNoCancel():
+    messagebox.askyesnocancel(title="Yes No Cancel", message="This is yes no cancel message box!")
+
+
+def askQuestion():
+    messagebox.askquestion(title="Question?", message="This is question message box!")
+
+
+def askRetryCancel():
+    messagebox.askretrycancel(title="Retry", message="This is retry message box!")
 
 
 windowExample = Tk()
 
-buttonExample = Button(text="Button Example", command=click, font=("Ariel", 22, "italic"), fg="blue", bg="black")
-buttonExample.pack()
+buttonInfo = Button(text="Button Info", command=info, font=("Ariel", 22, "italic"), fg="blue", bg="black")
+buttonInfo.pack()
+
+buttonWarning = Button(text="Button Warning", command=warning, font=("Ariel", 22, "italic"), fg="blue", bg="black")
+buttonWarning.pack()
+
+buttonError = Button(text="Button Info", command=error, font=("Ariel", 22, "italic"), fg="blue", bg="black")
+buttonError.pack()
+
+
+buttonYesNoCancel = Button(text="Button Yes No Cancel", command=yesNoCancel, font=("Ariel", 22, "italic"), fg="blue", bg="black")
+buttonYesNoCancel.pack()
+
+buttonQuestion = Button(text="Button Ask Question", command=askQuestion, font=("Ariel", 22, "italic"), fg="blue", bg="black")
+buttonQuestion.pack()
+
+buttonRetry = Button(text="Button Retry", command=askRetryCancel, font=("Ariel", 22, "italic"), fg="blue", bg="black")
+buttonRetry.pack()
 
 windowExample.mainloop()
