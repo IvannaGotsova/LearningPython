b97b0a595217556bbcf43af1871c530f3744112d
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-28 14:27:09 +0300

    Label Basic

---
+++ Graphic User Interface (GUI)/Label.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+windowsExample = Tk()
+
+labelExample = Label(text="Label Example", font=("Ariel", 22, "italic"), fg="blue", bg="black")
+labelExample.pack()
+
+windowsExample.mainloop()
