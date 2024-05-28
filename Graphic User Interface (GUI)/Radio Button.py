93677a384283b68a84016c97a048e244f4d6233f
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-28 16:40:40 +0300

    Radio Box Modified

--- Graphic User Interface (GUI)/Radio Button.py
+++ Graphic User Interface (GUI)/Radio Button.py
@@ -3,12 +3,13 @@ from tkinter import *
 
 optionsExample = ["one", "second", "third", "fourth", "fifth", "sixth", "seventh" ]
 
+
 windowExample = Tk()
 
 variableExample = StringVar()
 
 for i in range(len(optionsExample)):
-    radioButtonExample = Radiobutton(text=optionsExample[i], variable=variableExample, value=i, padx=10, )
+    radioButtonExample = Radiobutton(text=optionsExample[i], variable=variableExample, value=i, padx=10)
     radioButtonExample.pack()
 
 windowExample.mainloop()
