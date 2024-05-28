efcd95671dfb08ef930ee0c513429e4fc3090fbf
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-28 15:30:57 +0300

    Check Box Basic

---
+++ Graphic User Interface (GUI)/CheckBox.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+
+windowExample = Tk()
+
+checkBoxExample = Checkbutton(text="Check Box", font=("Ariel", 22, "italic"), fg="blue", bg="black")
+checkBoxExample.pack()
+
+windowExample.mainloop()
