9181c61b2a7429622b8b059b5a731511043656d0
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-28 18:58:26 +0300

    Text Box Modified

--- Graphic User Interface (GUI)/TextBox.py
+++ Graphic User Interface (GUI)/TextBox.py
@@ -11,7 +11,7 @@ windowExample = Tk()
 textExample = Text(font=("Ariel", 22, "italic"), fg="blue", bg="black")
 textExample.pack()
 
-buttonText = Button(windowExample, text="Button Text", command=text)
+buttonText = Button(windowExample, text="Button Text", command=text, font=("colibri", 22, "italic"), fg="blue", bg="black", state="active")
 buttonText.pack()
 
 
