0bcfc15b7fb612a3ee82647a976bcd11d84d751f
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-28 13:59:03 +0300

    Window Basic

---
+++ Graphic User Interface (GUI)/Window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+windowsExample = Tk()
+
+windowsExample.geometry("500x500")
+windowsExample.title("Window Example")
+windowsExample.config(background="grey")
+
+windowsExample.mainloop()
