8e1cc5bcacbfaae6e37ae2e84856e2aa358f8a8f
IvannaGotsova <ivannagotsova@outlook.com>
2024-12-19 21:06:16 +0200

    DataFrame

---
+++ Pandas/DataFrame.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = {
+  "first_column": [1, 2, 3, 4, 5],
+  "second_column": [10, 20, 30, 40, 50],
+  "third_column": [100, 200, 300, 400, 500]
+}
+
+dataFrame = pd.DataFrame(data, index = ["first_row", "second_row", "third_row", "fourth_row", "fifth_row"])
+
+print(dataFrame)
+
+print(dataFrame.loc[["first_row", "third_row"]])
+
+
+
