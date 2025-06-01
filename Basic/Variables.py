9f064b5b9fe1dd9dc02d3b06b280c82dd3b10ee3
IvannaGotsova <ivannagotsova@outlook.com>
2025-06-01 11:59:31 +0300

    Basic

---
+++ Basic/Variables.py
@@ -0,0 +1,20 @@
+from _cffi_backend import typeof
+
+x = 5
+y = 5.5
+z = "string"
+
+x1, x2, x3 = "One", "Two", "Three"
+x4 = x5 = x6 = "Number"
+
+print(x)
+print(y)
+print(z)
+print(x1)
+print(x2)
+print(x3)
+print(x4, x5, x6)
+
+print(type(x))
+print(type(y))
+print(type(z))
